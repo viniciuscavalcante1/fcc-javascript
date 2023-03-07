fc2fc38e54365d4cce373aafd5ef9119e198259c
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 15:23:12 -0300

    Add bracket - length

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ let firstLetterFirstName = "";
 // Javascript follows zero-based indexing, which means the first index number is 0.
 firstLetterFirstName = FIRSTNAME[0];
 console.log(firstLetterFirstName);
+
+let lastLetterFirstName = "";
+lastLetterFirstName = FIRSTNAME[FIRSTNAME.length - 1];
+console.log(lastLetterFirstName);
