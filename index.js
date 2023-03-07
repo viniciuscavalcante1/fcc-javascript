b233c6601d7501935789a45f29eb4a124624b610
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 15:30:23 -0300

    Add dialogue

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 const FIRSTNAME = "Vinícius";
 const LASTNAME = "Cavalcante";
 
+console.log("My full name is " + FIRSTNAME + " " + LASTNAME + ".");
+
 let firstLetterFirstName = "";
 
 // Javascript follows zero-based indexing, which means the first index number is 0.
 firstLetterFirstName = FIRSTNAME[0];
-console.log(firstLetterFirstName);
+console.log("The first letter of my first name is " + firstLetterFirstName + ".");
 
 let lastLetterFirstName = "";
 lastLetterFirstName = FIRSTNAME[FIRSTNAME.length - 1];
-console.log(lastLetterFirstName);
+console.log("The last letter of my first name is " + lastLetterFirstName + ".");
