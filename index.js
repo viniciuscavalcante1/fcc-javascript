a526a14305ec178923ec900615006307e60a5c20
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 17:56:30 -0300

    Add or operator

--- index.js
+++ index.js
@@ -174,3 +174,17 @@ function testLogicalAnd(val) {
   }
   
 testLogicalAnd(10);
+
+// or operator
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val < 10 || val > 20) {
+      return "Outside";
+    }
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+testLogicalOr(15);
