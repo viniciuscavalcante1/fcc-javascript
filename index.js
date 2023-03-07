24e4ed09bf59b8c552af2206fc4ac36df94886fa
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 17:48:06 -0300

    Add greater then

--- index.js
+++ index.js
@@ -131,3 +131,18 @@ function testStrictNotEqual(val) {
   }
   
   testStrictNotEqual(10);
+
+// greater then
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
