d2c6f49dd8fa74bdc393aaef8cc4c17c2df02a86
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 17:57:56 -0300

    Add else if

--- index.js
+++ index.js
@@ -188,3 +188,16 @@ function testLogicalOr(val) {
   }
   
 testLogicalOr(15);
+
+// else if
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else {
+    return "Between 5 and 10"; 
+    }
+  }
+  
+testElseIf(7);
