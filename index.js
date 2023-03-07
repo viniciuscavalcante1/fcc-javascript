3366e2ecf2e5085e90dc714edf234f0f37ad956a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 17:47:05 -0300

    Add strict inequality

--- index.js
+++ index.js
@@ -120,3 +120,14 @@ function testNotEqual(val) {
   }
   
   testNotEqual(10);
+
+// strict inequality
+// Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
