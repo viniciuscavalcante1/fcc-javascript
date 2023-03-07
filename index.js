4822e587ec7d4f62e247d533769c92d4c9dfd40a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 15:16:44 -0300

    Add zero-based indexing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+// const is a constant, which is always good to uppercase it.
+
+const FIRSTNAME = "Vinícius";
+const LASTNAME = "Cavalcante";
+
+let firstLetterFirstName = "";
+
+// Javascript follows zero-based indexing, which means the first index number is 0.
+firstLetterFirstName = FIRSTNAME[0];
+console.log(firstLetterFirstName);
