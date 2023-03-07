164c01e2fb0cfb460b2c30121845f7966c026978
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-07 17:53:10 -0300

    Add and operator

--- index.js
+++ index.js
@@ -160,3 +160,17 @@ function testGreaterOrEqual(val) {
 }
 
 testGreaterOrEqual(10);
+
+// and operator &&
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+testLogicalAnd(10);
