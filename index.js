b4e255db1ca20e004ee83f87f4de9bd1abcd072b
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-14 11:15:00 -0300

    Add bolean return

--- index.js
+++ index.js
@@ -361,3 +361,12 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+// Return with boolean
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b
+  // Only change code above this line
+}
+
+isLess(10, 15);
