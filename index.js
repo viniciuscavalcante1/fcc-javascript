f654fad572cbd40fae2e67865887d1801e3efc87
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 11:44:16 -0300

    Add array for iteration

--- index.js
+++ index.js
@@ -678,3 +678,13 @@ const myArray = [];
 for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+
+// iterating through array
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
