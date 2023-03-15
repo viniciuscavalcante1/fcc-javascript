8ddf4030586299553fd547105dec8e85885f61cd
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 11:32:53 -0300

    add for loop

--- index.js
+++ index.js
@@ -651,3 +651,12 @@ while (i >= 0) {
   i--;
 }
 // Only change code below this line
+
+// for loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
