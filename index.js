f351cab95ffaeeaeb327e798572930902e833162
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 10:47:55 -0300

    Add object properties access using brackets

--- index.js
+++ index.js
@@ -445,3 +445,15 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
+
+// Object access using brackets
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
