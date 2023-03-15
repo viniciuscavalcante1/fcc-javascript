3088dd6591c2ff9641305de8fd8a802260b1cd38
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 11:14:17 -0300

    add accessing nestng arrays

--- index.js
+++ index.js
@@ -579,3 +579,25 @@ const myStorage = {
  };
   
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// add accessing nestng arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
