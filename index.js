c99e5b3b62985ef5dd79c7de7db9bcc5ae420bfd
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 10:40:54 -0300

    Add case and if examples

--- index.js
+++ index.js
@@ -386,3 +386,40 @@ function abTest(a, b) {
 }
 
 abTest(2,2);
+
+// Case and if examples
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+  }
+
+  if (count > 0) {
+    return (count + " Bet")
+  } else {
+    return (count + " Hold")
+  }
+  return ;
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
