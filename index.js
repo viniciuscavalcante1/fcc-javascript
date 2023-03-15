c1db3c9387f3c0ffee7f036df28daf6de56fa87e
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 10:44:45 -0300

    Add objects

--- index.js
+++ index.js
@@ -423,3 +423,13 @@ function cc(card) {
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// objects
+const myDog = {
+  // Only change code below this line
+  "name": "Elvis",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Vini", "Mil", "Soc", "Lin"]
+  // Only change code above this line
+};
