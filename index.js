340c9d09f368ea790ba39f76549e6a5135adbe5e
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 11:35:23 -0300

    add for with 2 increment

--- index.js
+++ index.js
@@ -660,3 +660,12 @@ const myArray = [];
 for (let i = 1; i <= 5; i++) {
   myArray.push(i);
 }
+
+// for with 2 increment
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
