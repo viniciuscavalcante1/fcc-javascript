06bec897758a759d3a16efddc56f5ab7964788bb
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 10:52:31 -0300

    Add delete object properties

--- index.js
+++ index.js
@@ -491,3 +491,16 @@ const myDog = {
 };
 
 myDog.bark = "woof"
+
+// delete object properties
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails
