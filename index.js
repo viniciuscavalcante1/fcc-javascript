8e0a7f0e4f2b73ffc731b7ef38bb91bccdee76e4
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 11:06:00 -0300

    Add check if object has propertie with hasOwnProperty

--- index.js
+++ index.js
@@ -527,3 +527,14 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+// check if object has propertie with hasOwnProperty
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found"
+  }
+  // Only change code above this line
+}
