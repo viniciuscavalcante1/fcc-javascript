5436b0d2685f1c83d4621d873a4ec7970bc8a2d7
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 14:38:56 -0300

    add iterating through multidimensional array

--- index.js
+++ index.js
@@ -688,3 +688,19 @@ let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+// iterating through multidimensional array
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j] 
+    }
+
+  }
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
