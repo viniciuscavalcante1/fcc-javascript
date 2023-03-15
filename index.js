af69e214a7a08e83db76de22beb328076bca0771
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-03-15 10:45:57 -0300

    Add object propertie access

--- index.js
+++ index.js
@@ -433,3 +433,15 @@ const myDog = {
   "friends": ["Vini", "Mil", "Soc", "Lin"]
   // Only change code above this line
 };
+
+// Accessing objects properties
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
